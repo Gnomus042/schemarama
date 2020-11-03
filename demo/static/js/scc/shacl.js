a81bc2b1f99b6efa27f5d9d0c22281001f05a357
Anastasiia Byvsheva <gnomus042@gmail.com>
2020-11-03 16:11:38 +0200

    shacl message bug correction

--- demo/static/js/scc/shacl.js
+++ demo/static/js/scc/shacl.js
@@ -18,6 +18,8 @@ async function validateShacl(data, baseUrl) {
     let report = await shaclValidator.validate(data, {baseUrl: baseUrl});
     report.failures.forEach(failure => {
         failure.service = shapeToService[failure.shape.replace('http://example.org/', '')];
+        failure.message = failure.message.split('<').join('')
+            .split('>').join('').replace('http://example.org/', '');
     });
     removeDisabled(hierarchy, report);
     return report;
